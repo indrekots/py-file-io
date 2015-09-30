9e5c51aa80a6676232a48e5798654f67d75ca884
Indrek Ots <ots.indrek@gmail.com>
2015-09-30 18:51:17 +0300

    count unique words, append to collection

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import sys
 from collections import Counter
 
 def main():
-    files = sys.argv[1:]
-    f = open('file2.txt', 'r')
-    print(f.read())
+    csv_rows = []
+    file_names = sys.argv[1:]
+    for name in file_names:
+        try:
+            with open(name, 'r') as f:
+                append_row(name, f, csv_rows)
+        except FileNotFoundError:
+            print("%s does not exist" % name)
+
+    print(csv_rows)
 
 def unique_words(text):
     return len(Counter(text))
+
+def append_row(filename, file, rows):
+    rows.append({"name": filename, "unique_words": unique_words(file.read())})
 
 if __name__ == "__main__":
     main()
